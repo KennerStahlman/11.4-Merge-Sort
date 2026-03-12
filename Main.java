70a936d13fbf45ebdbe3c5cbc304d9f02cf1e8b2
KennerStahlman <Kenner.stahlman@outlook.com>
2026-03-12 09:47:21 -0500

    done

--- Main.java
+++ Main.java
@@ -12,11 +12,11 @@ public class Main {
     }
 
     public static void mergeSort(int[] array) {
-        // Your code here
+        MergeSort. mergeSort(array);
     }
 
     public static void merge(int[] array, int[] left, int[] right) {
-        // Your code here
+        MergeSort.merge(array, left, right);
     }
 
     public static void printArray(int[] array) {
@@ -24,5 +24,42 @@ public class Main {
             System.out.print(num + " ");
         }
         System.out.println();
+    }
+}
+
+class MergeSort {
+    public static void mergeSort(int[] array) {
+        if (array.length == 1){
+            return;
+        }
+        int mid = array.length / 2;
+        int[] left = new int[mid];
+        int[] right = new int[array.length - mid];
+        for (int i = 0; i < mid; i++) {
+            left[i] = array[i];
+        }
+        for (int i = 0; i < array.length - mid; i++) {
+            right[i] = array[mid + i];
+        }
+        mergeSort(left);
+        mergeSort(right);
+        merge(array, left, right);
+
+    }
+    public static void merge(int[] array, int[] left, int[] right) {
+        int i = 0, j = 0, k = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                array[k++] = left[i++];
+            } else {
+                array[k++] = right[j++];
+            }
+        }
+        while (i < left.length) {
+            array[k++] = left[i++];
+        }
+        while (j < right.length) {
+            array[k++] = right[j++];
+        }
     }
 }
